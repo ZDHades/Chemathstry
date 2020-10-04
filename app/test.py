8f83914363d4fa517225b429ec09984c9680abf3
ZDHades <npatel65799@gmail.com>
2020-10-04 15:57:57 -0400

    changes

--- app/test.py
+++ app/test.py
@@ -1,4 +1,4 @@
-from chempy import balance_stoichiometry
+from chempy import balance_stoichiometry, Substance
 from pprint import pprint
 # reac, prod = balance_stoichiometry(['NH4ClO4', 'Al'], ['Al2O3', 'HCl', 'H2O', 'N2'])
 
@@ -10,7 +10,20 @@ from pprint import pprint
     # placeholder for products = 
     
 
-reac, prod = balance_stoichiometry(['NH4ClO4', 'Al'], ['Al2O3', 'HCl', 'H2O', 'N2'])
-solution = (f'{str(reac)[13:-2]} = {str(prod)[13:-2]}')
+# reac, prod = balance_stoichiometry(['NH4ClO4', 'Al'], ['Al2O3', 'HCl', 'H2O', 'N2'])
+# solution = (f'{str(reac)[13:-2]} = {str(prod)[13:-2]}')
 
-print(solution)
+prod = ['Al2O3', 'HCl', 'H2O', 'N2']
+prods = []
+reac = ['NH4ClO4', 'Al']
+reacs = []
+
+for chem in prod:
+    Name = Substance.from_formula(chem)
+    prods.append(Name.unicode_name)
+    # print(Name.unicode_name)
+for chem in reac:
+    Name = Substance.from_formula(chem)
+    reacs.append(Name.unicode_name)
+    # print(Name.unicode_name)
+a, b = balance_stoichiometry(reacs, prods)
